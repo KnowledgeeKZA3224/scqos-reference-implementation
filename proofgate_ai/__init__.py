38aa78d2fd8bd5c247a91272f669647075b6d9f9
Knowledgee KZA (Architect) <erobles3224@gmail.com>
2026-09-13 18:40:02 -0700

    Add ProofGate Python package

---
+++ proofgate_ai/__init__.py
@@ -0,0 +1,5 @@
+"""ProofGate AI client for the SCQOS public pre-execution boundary."""
+from .client import ProofGateClient, ProofGateError
+
+__all__ = ["ProofGateClient", "ProofGateError"]
+__version__ = "0.1.0"
